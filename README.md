de3b5adaca0d37bfd0999713e141e5f0ce65e34e
initdc <initd@outlook.com>
2022-02-28 21:42:20 +0800

    chore: rewrite with better var assigned

--- README.md
+++ README.md
@@ -6,32 +6,25 @@
 
 The Workers includes limited KV Usage. Listing value will cause a lot of read usage and list usage.
 
- You should ensure that you are aware of this, and it might be a good idea to add validation before the operation.
+You should ensure that you are aware of this, and it might be a good idea to add validation before the operation.
 
 ## Deploy
 
 1. create a worker
 
-
-2. paste the code from ```index.js```
-
+2. paste the code from `index.js`
 
 3. set KV database binding
 
-
 4. create records in KV database
 
+5. set route for `*.example.com`
 
-5. set route for ```*.example.com```
-
-
-6. create DNS record for ```*.example.com``` 
-
+6. create DNS record for `*.example.com`
 
 ## Reference
 
 [Workers KV API reference](https://developers.cloudflare.com/workers/runtime-apis/kv)
-
 
 ## Powered by
 
--- index.js
+++ index.js
@@ -1,57 +1,41 @@
-addEventListener("fetch", event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  String.prototype.rjust = function( length, char ) {
-      var fill = [];
-      while ( fill.length + this.length < length ) {
-        fill[fill.length] = char;
-      }
-      return fill.join('') + this;
+const NS = SUB;
+
+const base = ".example.com";
+const wrapper = 20;
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest()));
+
+String.prototype.rjust = function (length, char) {
+  var fill = [];
+  while (fill.length + this.length < length) {
+    fill[fill.length] = char;
   }
-  
-  String.prototype.ljust = function( length, char ) {
-      var fill = [];
-      while ( fill.length + this.length < length ) {
-        fill[fill.length] = char;
-      }
-      return this + fill.join('');
+  return fill.join("") + this;
+};
+
+String.prototype.ljust = function (length, char) {
+  var fill = [];
+  while (fill.length + this.length < length) {
+    fill[fill.length] = char;
   }
-  
-  async function listKeys(namespace) {
-    const value = await namespace.list()
-  
-    return value
+  return this + fill.join("");
+};
+
+async function handleRequest() {
+  // await NS.put("a", "apple");
+
+  const list = await NS.list();
+  const keys = list.keys;
+
+  if (!keys) {
+    return new Response("List is empty", { status: 404 });
   }
-  
-  async function getValue(namespace, key) {
-    const value = await namespace.get(key)
-    if (value === null) {
-      return new Response('Value not found', {status: 404})
-    }
-  
-    return value
+
+  let content = new String();
+  for (const key of keys) {
+    const name = key.name;
+    const value = (await NS.get(name)) || "Value not found";
+    content += (name + base).ljust(wrapper, " ") + value + "\n\n";
   }
-  
-  async function handleRequest(request) {
-    const list = await listKeys(SUB)
-    let error = 'List has no keys'
-    let base = '.example.com'
-    let biggest = 20
-    let output = new String
-    let key = new String
-    let value = new String
-  
-    if (list.keys.length > 0){
-      const length = list.keys.length
-      for (let i = 0 ; i < length; i++){
-        key =  list.keys[i].name
-        value = await getValue(SUB, key)
-        output += (key + base).ljust(biggest, ' ') + value + '\n\n'
-      }
-      return new Response(output, {status: 200})
-    }
-  
-    return new Response(error, {status: 404})
-  }
-  
+  return new Response(content, { status: 200 });
+}
