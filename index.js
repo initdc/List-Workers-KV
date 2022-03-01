083a076690028214250c85c59030344dd106aeaa
initdc <initd@outlook.com>
2022-03-01 14:33:43 +0800

    feat(post): add post for getting stored content

--- index.js
+++ index.js
@@ -7,23 +7,21 @@ async function handleRequest() {
   // await NS.put("a", "apple");
 
   const list = await NS.list();
-  const keys = list.keys;
 
+  const keys = list.keys;
   if (!keys) {
     return new Response("List is empty", { status: 404 });
   }
 
   const length = keys.length;
-
   for (let i = 0; i < length; i++) {
     const name = keys[i].name;
     const value = await NS.get(name);
     keys[i].value = value;
   }
-  const content = JSON.stringify(keys);
 
+  const content = JSON.stringify(keys);
   await STORE.put("sub-list", content);
   // const storeContent = await STORE.get("sub-list");
-
   return new Response(content, { status: 200 });
 }
